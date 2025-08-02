122dbee773a20fffaa652a79e3bf452792629bc7
adamp <adam.piskorek@gmx.de>
2025-08-02 20:35:50 +0200

    filter function added

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,13 @@
 function fLetterUp(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+function findByName () {
+  document.getElementById('findId').classList.toggle('displayNone')
+  document.getElementById('findValueId').value=''
+}
+
+function closeFind(){
+  document.getElementById('findValueId').value=''
+  document.getElementById('findId').classList.toggle('displayNone')
+}
--- scripts/render.js
+++ scripts/render.js
@@ -1,44 +1,57 @@
+async function findTyp(x) {
+  let myDiv = document.getElementById("pokeCardId");
+  for (i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].typeSlot1 == x || pokemons[i].typeSlot2 == x) {
+      document.getElementById("pokeCardId" + i).classList.remove("displayNone");
+    } else {
+      document.getElementById("pokeCardId" + i).classList.add("displayNone");
+    }
+  }
+}
+
+async function find(lastChar) {
+  let myName = document.getElementById("findValueId").value;
+  myName = lastChar.length < 2 ? myName + lastChar : myName.slice(0, -1);
+  if (myName.length > -1) {
+    for (i = 0; i < pokemons.length; i++) {
+      if (pokemons[i].name.match(myName.toLowerCase())) {
+        document
+          .getElementById("pokeCardId" + i)
+          .classList.remove("displayNone");
+      } else {
+        document.getElementById("pokeCardId" + i).classList.add("displayNone");
+      }
+    }
+  }
+}
+
 async function myPokemonList(n) {
   let myDiv = document.getElementById("pokedex");
   myDiv.innerHTML = "";
   for (i = 0; i < n; i++) {
-    myDiv.innerHTML +=template(i)
+    myDiv.innerHTML += template(i);
   }
 }
 
-async function findTyp(x) {
-  let myDiv = document.getElementById("pokedex");
-  myDiv.innerHTML = "";
-  for (i = 0; i < pokemons.length; i++) {
-    if(pokemons[i].typeSlot1==x||pokemons[i].typeSlot2==x){
-    myDiv.innerHTML +=template(i)}
-  }
-}
-
-async function find() {
-  let myDiv = document.getElementById("pokedex");
-  myDiv.innerHTML = "";
-  for (i = 0; i < pokemons.length; i++) {
-    if(pokemons[i].typeSlot1==x){
-    myDiv.innerHTML +=template(i)}
-  }
-}
-
-
-function template(i){
-  x= `
+function template(i) {
+  x = `
  <div id="pokeCardId${i}" class="pokeCard ${pokemons[i].typeSlot1}">
         <div class="cardHeader">   
-            <p>Nr.${i+1}</p>
+            <p>Nr.${i + 1}</p>
         </div>
-        <img id="imageLargeId${i}" class="imageLarge" src="${pokemons[i].imageLarge}">
-        <p class="cardName">${fLetterUp(pokemons[i].name)}</p>
+        <img id="imageLargeId${i}" class="imageLarge" src="${
+    pokemons[i].imageLarge
+  }">
+        <p id="cardNameId${i}">${fLetterUp(pokemons[i].name)}</p>
         <div class="cardBottom">
-        <p class="cardType ${pokemons[i].typeSlot1}" >${pokemons[i].typeSlot1}</p>
-        <p class="${(pokemons[i].typeSlot2? 'cardType':'')} ${(pokemons[i].typeSlot2? pokemons[i].typeSlot2:'')}" >${(pokemons[i].typeSlot2? pokemons[i].typeSlot2:'')}</p>
+        <p class="cardType ${pokemons[i].typeSlot1}" >${
+    pokemons[i].typeSlot1
+  }</p>
+        <p class="${pokemons[i].typeSlot2 ? "cardType" : ""} ${
+    pokemons[i].typeSlot2 ? pokemons[i].typeSlot2 : ""
+  }" >${pokemons[i].typeSlot2 ? pokemons[i].typeSlot2 : ""}</p>
         </div>
  </div> 
  `;
-
- return x
+  return x;
 }
