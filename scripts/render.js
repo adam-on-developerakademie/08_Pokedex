42e08f9ef68f1cf6ec94f85ba6ed784ff884f3c4
adamp <adam.piskorek@gmx.de>
2025-08-02 02:43:16 +0200

    searche added

--- scripts/render.js
+++ scripts/render.js
@@ -2,19 +2,46 @@ async function myPokemonList(n) {
   let myDiv = document.getElementById("pokedex");
   myDiv.innerHTML = "";
   for (i = 0; i < n; i++) {
-    myDiv.innerHTML += `
+    myDiv.innerHTML +=template(i)
+  }
+}
+
+async function findTyp(x) {
+  let myDiv = document.getElementById("pokedex");
+  myDiv.innerHTML = "";
+  for (i = 0; i < pokemons.length; i++) {
+    if(pokemons[i].typeSlot1==x){
+    myDiv.innerHTML +=template(i)}
+  }
+}
+
+async function find() {
+  let myDiv = document.getElementById("pokedex");
+  myDiv.innerHTML = "";
+  for (i = 0; i < pokemons.length; i++) {
+    if(pokemons[i].typeSlot1==x){
+    myDiv.innerHTML +=template(i)}
+  }
+}
+
+
+
+
+
+function template(i){
+  x= `
  <div id="pokeCardId${i}" class="pokeCard ${pokemons[i].typeSlot1}">
         <div class="cardHeader">   
             <p>Nr.${i+1}</p>
         </div>
-            
         <img id="imageLargeId${i}" class="imageLarge" src="${pokemons[i].imageLarge}">
         <p class="cardName">${fLetterUp(pokemons[i].name)}</p>
         <div class="cardBottom">
-        <p class="cardType ${pokemons[i].typeSlot1}Type" >${pokemons[i].typeSlot1}</p>
-        <p class="cardType" >${(pokemons[i].typeSlot2? pokemons[i].typeSlot2:'')}</p>
+        <p class="cardType ${pokemons[i].typeSlot1}" >${pokemons[i].typeSlot1}</p>
+        <p class="${(pokemons[i].typeSlot2? 'cardType':'')} ${(pokemons[i].typeSlot2? pokemons[i].typeSlot2:'')}" >${(pokemons[i].typeSlot2? pokemons[i].typeSlot2:'')}</p>
         </div>
  </div> 
  `;
-  }
+
+ return x
 }
